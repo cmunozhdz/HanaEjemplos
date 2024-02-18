ae54171c8d1498ed1fe8ec092a6c7f3c93d17519
clemente en ubuntu <cmunoz_hernandez@hotmail.com>
2024-02-18 00:21:22 -0600

    Convirtiendo proyecto a maven

--- ClienteHana/src/ClienteHana.java
+++ ClienteHana/src/ClienteHana.java
@@ -13,10 +13,11 @@ public class ClienteHana {
 			//1f3a4df5-3630-46ab-a410-ad0562f9b40d
 			String SchemasName;
 			System.out.println("Conectando a Hana");
+			//Agregamos la conexion al schema.
 			String ServerConnection= "1f3a4df5-3630-46ab-a410-ad0562f9b40d.hana.trial-us10.hanacloud.ondemand.com:443";	
 			String Usuario= "DBADMIN";
 			String Password = "Kl3m3n73$$";
-			String JdbConnection = "jdbc:sap://" + ServerConnection + "?encrypt=true&validateCertificate=false " ; 		
+			String JdbConnection = "jdbc:sap://" + ServerConnection + "?encrypt=true&validateCertificate=false&currentSchema=PWP" ; 		
 			connection = DriverManager.getConnection(JdbConnection,Usuario,Password);                
 			
 			
@@ -33,7 +34,7 @@ public class ClienteHana {
 				
 				System.out.println("Conexion establecida");
 				Statement Stmt =connection.createStatement();
-				ResultSet Rst = Stmt.executeQuery("Select * from PWP.CFDI");
+				ResultSet Rst = Stmt.executeQuery("Select * from CFDI");
 				
 				while ( Rst.next() ) {
 					
